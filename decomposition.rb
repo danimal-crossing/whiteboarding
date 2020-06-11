855cd3cd3c71e6ad76b4ecc613c56c904052a176
dani <dani.zaghian@gmail.com>
2020-06-11 14:03:35 -0700

    adds decomposition

--- decomposition.rb
+++ decomposition.rb
@@ -9,3 +9,14 @@
 # Write a function that takes in 2 strings, split each into an array, loops through each array, and prints out each letter of each array
 # Write a function that takes in 2 strings, loops through each letter of the first string, and compare the letter to each letter of the second string. Print the word "match" if they match
 
+
+
+# Write a function that takes in an array of numbers and returns a new array of the same numbers without any duplicates.
+
+# example input: 
+# my_array = [1, 2, 22, 3, 3, 4, 5, 2, -1]
+# example output: [1, 2, 22, 3, 4, 5, -1]
+
+# Write a function that takes in an array of numbers and prints it
+# Write and then loops through an array of numbers and prints out each number individually
+# Write a function that outer loops through an array of numbers and then inner loops through the same array of numbers and compares the "current number" with each other number and if they match print "true"
