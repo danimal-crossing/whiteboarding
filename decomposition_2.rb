ac1ca675f1063e4ec237e86eb48e40c169b637e6
dani <dani.zaghian@gmail.com>
2020-06-25 13:42:33 -0700

    adds decomposition problems

---
+++ decomposition_2.rb
@@ -0,0 +1,22 @@
+# Write a function that takes in a string of code with parentheses, square brackets, and curly brackets, and checks if the code is balanced with correct bracket syntax.
+
+# expected input: 
+# "(x + 1)["
+# "(x + [1, {a: 2, b: 3}])"
+# expected output: 
+# false
+# true
+
+# Write a function that takes in a string and prints out the characters of the string one at a time
+# Write a function that takes in a string and counts how many open parenthesis are in the string
+# Write a function that takes in a string and counts how many open parenthesis are in the string and how many close parenthesis (and the same for curly brackets and square brackets)
+
+
+
+# Write a function that takes in two arrays of numbers and returns all the numbers that appear in both arrays.
+
+# expected input: [2, 3, 7, 18, 0] [4, 6, 7, 2, 0]
+# expected output: {2: true, 7: true, 0: true}
+
+# Write a function that takes in 2 arrays and loops and prints out all the elements in the each array
+# Write a function that does a nested loop and checks each element in the outer array against each element in the inner array
